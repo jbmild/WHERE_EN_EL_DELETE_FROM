47cc1e8d3c5adff2207b04f56fb3f6e6ea56853c
Brenda Stolarz <brenda.stolarz@gmail.com>
2018-05-31 23:34:23 -0300

    vuelvo a incluir el abm habitacion en la solucion

---
+++ FrbaHotel/darAltaHabitacion.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace FrbaHotel.AbmHabitacion
+{
+    partial class darAltaHabitacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxHotel = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxNroHabitacion = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxPisoEnHotel = new System.Windows.Forms.ComboBox();
+            this.checkBoxVistaExterior = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxTipoHabitacion = new System.Windows.Forms.ComboBox();
+            this.textBoxDescripcionHabitacion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCrearHabitacion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hotel:";
+            // 
+            // comboBoxHotel
+            // 
+            this.comboBoxHotel.FormattingEnabled = true;
+            this.comboBoxHotel.Location = new System.Drawing.Point(205, 39);
+            this.comboBoxHotel.Name = "comboBoxHotel";
+            this.comboBoxHotel.Size = new System.Drawing.Size(155, 24);
+            this.comboBoxHotel.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nro de habitación:";
+            // 
+            // comboBoxNroHabitacion
+            // 
+            this.comboBoxNroHabitacion.FormattingEnabled = true;
+            this.comboBoxNroHabitacion.Location = new System.Drawing.Point(239, 79);
+            this.comboBoxNroHabitacion.Name = "comboBoxNroHabitacion";
+            this.comboBoxNroHabitacion.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxNroHabitacion.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Piso dentro del hotel:";
+            // 
+            // comboBoxPisoEnHotel
+            // 
+            this.comboBoxPisoEnHotel.FormattingEnabled = true;
+            this.comboBoxPisoEnHotel.Location = new System.Drawing.Point(239, 115);
+            this.comboBoxPisoEnHotel.Name = "comboBoxPisoEnHotel";
+            this.comboBoxPisoEnHotel.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxPisoEnHotel.TabIndex = 5;
+            // 
+            // checkBoxVistaExterior
+            // 
+            this.checkBoxVistaExterior.AutoSize = true;
+            this.checkBoxVistaExterior.Location = new System.Drawing.Point(464, 215);
+            this.checkBoxVistaExterior.Name = "checkBoxVistaExterior";
+            this.checkBoxVistaExterior.Size = new System.Drawing.Size(165, 21);
+            this.checkBoxVistaExterior.TabIndex = 6;
+            this.checkBoxVistaExterior.Text = "Tiene vista al exterior";
+            this.checkBoxVistaExterior.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tipo habitación";
+            // 
+            // comboBoxTipoHabitacion
+            // 
+            this.comboBoxTipoHabitacion.FormattingEnabled = true;
+            this.comboBoxTipoHabitacion.Location = new System.Drawing.Point(239, 157);
+            this.comboBoxTipoHabitacion.Name = "comboBoxTipoHabitacion";
+            this.comboBoxTipoHabitacion.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxTipoHabitacion.TabIndex = 8;
+            // 
+            // textBoxDescripcionHabitacion
+            // 
+            this.textBoxDescripcionHabitacion.Location = new System.Drawing.Point(90, 271);
+            this.textBoxDescripcionHabitacion.Multiline = true;
+            this.textBoxDescripcionHabitacion.Name = "textBoxDescripcionHabitacion";
+            this.textBoxDescripcionHabitacion.Size = new System.Drawing.Size(378, 22);
+            this.textBoxDescripcionHabitacion.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Descripción";
+            // 
+            // btnCrearHabitacion
+            // 
+            this.btnCrearHabitacion.Location = new System.Drawing.Point(425, 356);
+            this.btnCrearHabitacion.Name = "btnCrearHabitacion";
+            this.btnCrearHabitacion.Size = new System.Drawing.Size(75, 23);
+            this.btnCrearHabitacion.TabIndex = 11;
+            this.btnCrearHabitacion.Text = "Crear";
+            this.btnCrearHabitacion.UseVisualStyleBackColor = true;
+            // 
+            // darAltaHabitacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 434);
+            this.Controls.Add(this.btnCrearHabitacion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDescripcionHabitacion);
+            this.Controls.Add(this.comboBoxTipoHabitacion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBoxVistaExterior);
+            this.Controls.Add(this.comboBoxPisoEnHotel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxNroHabitacion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxHotel);
+            this.Controls.Add(this.label1);
+            this.Name = "darAltaHabitacion";
+            this.Text = "Dar de alta habitacion";
+            this.Load += new System.EventHandler(this.darAltaHabitacion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxHotel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxNroHabitacion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxPisoEnHotel;
+        private System.Windows.Forms.CheckBox checkBoxVistaExterior;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxTipoHabitacion;
+        private System.Windows.Forms.TextBox textBoxDescripcionHabitacion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCrearHabitacion;
+    }
+}
