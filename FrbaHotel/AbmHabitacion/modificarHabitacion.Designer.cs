4d641d9d3231b2cea86efce5deb3b56515c90323
= <=>
2018-06-09 11:26:40 -0300

    abmModificarHabitacion en proceso

--- FrbaHotel/AbmHabitacion/modificarHabitacion.Designer.cs
+++ FrbaHotel/AbmHabitacion/modificarHabitacion.Designer.cs
@@ -32,22 +32,26 @@
             this.label2 = new System.Windows.Forms.Label();
             this.comboBoxNumeroHabitacion = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.labelHotelNuevo = new System.Windows.Forms.Label();
             this.comboBoxNuevoHotel = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.labelPisoNuevo = new System.Windows.Forms.Label();
             this.comboBoxNuevoPiso = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelDescripcionNueva = new System.Windows.Forms.Label();
             this.textBoxNuevaDescripcion = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBoxHoteles = new System.Windows.Forms.ComboBox();
+            this.checkBoxTieneVistaExterior = new System.Windows.Forms.CheckBox();
+            this.checkBoxEstaHabilitado = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelNuevosCambios = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(125, 20);
+            this.label1.Location = new System.Drawing.Point(41, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 17);
             this.label1.TabIndex = 0;
@@ -56,7 +60,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(125, 59);
+            this.label2.Location = new System.Drawing.Point(41, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(147, 17);
             this.label2.TabIndex = 2;
@@ -65,7 +69,7 @@
             // comboBoxNumeroHabitacion
             // 
             this.comboBoxNumeroHabitacion.FormattingEnabled = true;
-            this.comboBoxNumeroHabitacion.Location = new System.Drawing.Point(409, 59);
+            this.comboBoxNumeroHabitacion.Location = new System.Drawing.Point(251, 53);
             this.comboBoxNumeroHabitacion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxNumeroHabitacion.Name = "comboBoxNumeroHabitacion";
             this.comboBoxNumeroHabitacion.Size = new System.Drawing.Size(121, 24);
@@ -73,114 +77,170 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(519, 108);
+            this.button1.Location = new System.Drawing.Point(144, 90);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(153, 40);
             this.button1.TabIndex = 4;
             this.button1.Text = "Buscar ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label3
+            // labelHotelNuevo
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(81, 160);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 17);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Hotel";
+            this.labelHotelNuevo.AutoSize = true;
+            this.labelHotelNuevo.Location = new System.Drawing.Point(478, 211);
+            this.labelHotelNuevo.Name = "labelHotelNuevo";
+            this.labelHotelNuevo.Size = new System.Drawing.Size(41, 17);
+            this.labelHotelNuevo.TabIndex = 5;
+            this.labelHotelNuevo.Text = "Hotel";
+            this.labelHotelNuevo.Visible = false;
             // 
             // comboBoxNuevoHotel
             // 
             this.comboBoxNuevoHotel.FormattingEnabled = true;
-            this.comboBoxNuevoHotel.Location = new System.Drawing.Point(163, 158);
+            this.comboBoxNuevoHotel.Location = new System.Drawing.Point(560, 209);
             this.comboBoxNuevoHotel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxNuevoHotel.Name = "comboBoxNuevoHotel";
             this.comboBoxNuevoHotel.Size = new System.Drawing.Size(232, 24);
             this.comboBoxNuevoHotel.TabIndex = 6;
+            this.comboBoxNuevoHotel.Visible = false;
+            this.comboBoxNuevoHotel.SelectionChangeCommitted += new System.EventHandler(this.comboBoxNuevoHotel_SelectedIndexChanged);
             // 
-            // label4
+            // labelPisoNuevo
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(81, 206);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(105, 17);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Piso en el hotel";
+            this.labelPisoNuevo.AutoSize = true;
+            this.labelPisoNuevo.Location = new System.Drawing.Point(478, 257);
+            this.labelPisoNuevo.Name = "labelPisoNuevo";
+            this.labelPisoNuevo.Size = new System.Drawing.Size(105, 17);
+            this.labelPisoNuevo.TabIndex = 7;
+            this.labelPisoNuevo.Text = "Piso en el hotel";
+            this.labelPisoNuevo.Visible = false;
             // 
             // comboBoxNuevoPiso
             // 
             this.comboBoxNuevoPiso.FormattingEnabled = true;
-            this.comboBoxNuevoPiso.Location = new System.Drawing.Point(275, 206);
+            this.comboBoxNuevoPiso.Location = new System.Drawing.Point(672, 257);
             this.comboBoxNuevoPiso.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxNuevoPiso.Name = "comboBoxNuevoPiso";
             this.comboBoxNuevoPiso.Size = new System.Drawing.Size(121, 24);
             this.comboBoxNuevoPiso.TabIndex = 8;
+            this.comboBoxNuevoPiso.Visible = false;
             // 
-            // label5
+            // labelDescripcionNueva
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(83, 261);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 17);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Descripcion";
+            this.labelDescripcionNueva.AutoSize = true;
+            this.labelDescripcionNueva.Location = new System.Drawing.Point(480, 312);
+            this.labelDescripcionNueva.Name = "labelDescripcionNueva";
+            this.labelDescripcionNueva.Size = new System.Drawing.Size(82, 17);
+            this.labelDescripcionNueva.TabIndex = 9;
+            this.labelDescripcionNueva.Text = "Descripcion";
+            this.labelDescripcionNueva.Visible = false;
             // 
             // textBoxNuevaDescripcion
             // 
-            this.textBoxNuevaDescripcion.Location = new System.Drawing.Point(196, 261);
+            this.textBoxNuevaDescripcion.Location = new System.Drawing.Point(593, 312);
             this.textBoxNuevaDescripcion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxNuevaDescripcion.Multiline = true;
             this.textBoxNuevaDescripcion.Name = "textBoxNuevaDescripcion";
             this.textBoxNuevaDescripcion.Size = new System.Drawing.Size(199, 90);
             this.textBoxNuevaDescripcion.TabIndex = 10;
+            this.textBoxNuevaDescripcion.Visible = false;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(476, 158);
+            this.dataGridView1.Location = new System.Drawing.Point(418, 29);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(413, 194);
+            this.dataGridView1.Size = new System.Drawing.Size(1051, 80);
             this.dataGridView1.TabIndex = 11;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(425, 366);
+            this.button2.Location = new System.Drawing.Point(814, 448);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(169, 36);
             this.button2.TabIndex = 12;
             this.button2.Text = "Guardar cambios";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBoxHoteles
             // 
             this.comboBoxHoteles.FormattingEnabled = true;
-            this.comboBoxHoteles.Location = new System.Drawing.Point(228, 22);
-            this.comboBoxHoteles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxHoteles.Location = new System.Drawing.Point(144, 23);
+            this.comboBoxHoteles.Margin = new System.Windows.Forms.Padding(4);
             this.comboBoxHoteles.Name = "comboBoxHoteles";
             this.comboBoxHoteles.Size = new System.Drawing.Size(228, 24);
             this.comboBoxHoteles.TabIndex = 13;
             this.comboBoxHoteles.SelectionChangeCommitted += new System.EventHandler(this.comboBoxHoteles_SelectedIndexChanged);
             // 
+            // checkBoxTieneVistaExterior
+            // 
+            this.checkBoxTieneVistaExterior.AutoSize = true;
+            this.checkBoxTieneVistaExterior.Location = new System.Drawing.Point(483, 427);
+            this.checkBoxTieneVistaExterior.Name = "checkBoxTieneVistaExterior";
+            this.checkBoxTieneVistaExterior.Size = new System.Drawing.Size(165, 21);
+            this.checkBoxTieneVistaExterior.TabIndex = 14;
+            this.checkBoxTieneVistaExterior.Text = "Tiene vista al exterior";
+            this.checkBoxTieneVistaExterior.UseVisualStyleBackColor = true;
+            this.checkBoxTieneVistaExterior.Visible = false;
+            // 
+            // checkBoxEstaHabilitado
+            // 
+            this.checkBoxEstaHabilitado.AutoSize = true;
+            this.checkBoxEstaHabilitado.Location = new System.Drawing.Point(483, 463);
+            this.checkBoxEstaHabilitado.Name = "checkBoxEstaHabilitado";
+            this.checkBoxEstaHabilitado.Size = new System.Drawing.Size(123, 21);
+            this.checkBoxEstaHabilitado.TabIndex = 15;
+            this.checkBoxEstaHabilitado.Text = "Está habilitado";
+            this.checkBoxEstaHabilitado.UseVisualStyleBackColor = true;
+            this.checkBoxEstaHabilitado.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(628, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(404, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Aquí se mostrará la habitación correspondiente a su búsqueda";
+            // 
+            // labelNuevosCambios
+            // 
+            this.labelNuevosCambios.AutoSize = true;
+            this.labelNuevosCambios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNuevosCambios.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.labelNuevosCambios.Location = new System.Drawing.Point(371, 155);
+            this.labelNuevosCambios.Name = "labelNuevosCambios";
+            this.labelNuevosCambios.Size = new System.Drawing.Size(726, 25);
+            this.labelNuevosCambios.TabIndex = 17;
+            this.labelNuevosCambios.Text = "A continuación, puede modificar los siguientes datos de la habitación seleccionad" +
+    "a";
+            this.labelNuevosCambios.Visible = false;
+            // 
             // modificarHabitacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1061, 414);
+            this.ClientSize = new System.Drawing.Size(1505, 514);
+            this.Controls.Add(this.labelNuevosCambios);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBoxEstaHabilitado);
+            this.Controls.Add(this.checkBoxTieneVistaExterior);
             this.Controls.Add(this.comboBoxHoteles);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBoxNuevaDescripcion);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelDescripcionNueva);
             this.Controls.Add(this.comboBoxNuevoPiso);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelPisoNuevo);
             this.Controls.Add(this.comboBoxNuevoHotel);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelHotelNuevo);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBoxNumeroHabitacion);
             this.Controls.Add(this.label2);
@@ -195,20 +255,26 @@
 
         }
 
+        
+
         #endregion
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBoxNumeroHabitacion;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelHotelNuevo;
         private System.Windows.Forms.ComboBox comboBoxNuevoHotel;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelPisoNuevo;
         private System.Windows.Forms.ComboBox comboBoxNuevoPiso;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelDescripcionNueva;
         private System.Windows.Forms.TextBox textBoxNuevaDescripcion;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBoxHoteles;
+        private System.Windows.Forms.CheckBox checkBoxTieneVistaExterior;
+        private System.Windows.Forms.CheckBox checkBoxEstaHabilitado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelNuevosCambios;
     }
 }
--- FrbaHotel/AbmHabitacion/modificarHabitacion.cs
+++ FrbaHotel/AbmHabitacion/modificarHabitacion.cs
@@ -13,13 +13,23 @@ namespace FrbaHotel.AbmHabitacion
 {
     public partial class modificarHabitacion : Form
     {
+        string habitacion_numero;
+
         public modificarHabitacion()
         {
             InitializeComponent();
         }
 
-      
 
+        private void comboBoxNuevoHotel_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            ConexionSQL c = new ConexionSQL();
+            DataTable dtpisosNuevos = c.cargarTablaSQL("select distinct piso from WHERE_EN_EL_DELETE_FROM.habitaciones where hoteles_id=" +
+              this.comboBoxNuevoHotel.SelectedValue + " order by piso asc");
+            comboBoxNuevoPiso.DataSource = dtpisosNuevos;
+            comboBoxNuevoPiso.DisplayMember = "piso";
+            comboBoxNuevoPiso.ValueMember = "piso";
+        }
         private void modificarHabitacion_Load(object sender, EventArgs e)
         {
             ConexionSQL c = new ConexionSQL();
@@ -28,18 +38,58 @@ namespace FrbaHotel.AbmHabitacion
             comboBoxHoteles.DisplayMember = "direccion";
             comboBoxHoteles.ValueMember = "hotel_id";
 
-            DataTable dtpisos = c.cargarTablaSQL("select numero from WHERE_EN_EL_DELETE_FROM.habitaciones where hoteles_id=" + this.comboBoxHoteles.SelectedValue + " order by numero asc");
+            DataTable dtpisos = c.cargarTablaSQL("select habitacion_id, numero from WHERE_EN_EL_DELETE_FROM.habitaciones where hoteles_id=" + 
+                this.comboBoxHoteles.SelectedValue + " order by numero asc");
             comboBoxNumeroHabitacion.DataSource = dtpisos;
             comboBoxNumeroHabitacion.DisplayMember = "numero";
-            comboBoxNumeroHabitacion.ValueMember = "numero";
+            comboBoxNumeroHabitacion.ValueMember = "habitacion_id";
+
+
         }
 
         
         private void button1_Click(object sender, EventArgs e)
         {
-            ConexionSQL con=new ConexionSQL();
-            DataTable dtHabitacion = con.cargarTablaSQL("select numero, piso, convert(bit, frente) as Vista_Exterior, descripcion, convert(bit,habilitado) as Habilitado, tipos_id from WHERE_EN_EL_DELETE_FROM.habitaciones where numero=" + comboBoxNumeroHabitacion.SelectedValue + " and hoteles_id=" + comboBoxHoteles.SelectedValue);
-            dataGridView1.DataSource = dtHabitacion;
+            /*Hago visibles los demas componentes*/
+            this.VisibilizarComponentes();
+          
+
+            /*Guardo el numero de habitacion conocido por el empleado*/
+            habitacion_numero = comboBoxNumeroHabitacion.Text;
+
+            //textBoxNuevaDescripcion.Text = Convert.ToString(habitacion_numero);
+            ConexionSQL con2 = new ConexionSQL();
+            DataTable habitacionElegida = con2.cargarTablaSQL("select ha.numero as 'Numero_Habitacion', ha.piso as 'Piso', ha.descripcion as 'Descripcion'," +
+            "t.descripcion 'Tipo_Habitacion', convert(bit,ha.frente) as 'Tiene_vista_al_exterior', convert(bit,habilitado) as 'Esta_habilitada' from" +
+                " WHERE_EN_EL_DELETE_FROM.habitaciones ha join WHERE_EN_EL_DELETE_FROM.habitaciones_tipos t on ha.tipos_id=t.tipo_id  where habitacion_id=" + 
+                comboBoxNumeroHabitacion.SelectedValue);
+            
+            dataGridView1.DataSource = habitacionElegida;
+            dataGridView1.Columns[0].Width = 120;
+            dataGridView1.Columns[1].Width = 30;
+            dataGridView1.Columns[4].Width = 130;
+            ConexionSQL con = new ConexionSQL();
+            DataTable dtHotelesAct = con.cargarTablaSQL("select direccion, hotel_id from WHERE_EN_EL_DELETE_FROM.hoteles");
+            comboBoxNuevoHotel.DataSource = dtHotelesAct;
+            comboBoxNuevoHotel.DisplayMember = "direccion";
+            comboBoxNuevoHotel.ValueMember = "hotel_id";
+            DataTable dtPisosHotelAct = con.cargarTablaSQL("select distinct piso from WHERE_EN_EL_DELETE_FROM.habitaciones where hoteles_id=" + comboBoxNuevoHotel.SelectedValue);
+            comboBoxNuevoPiso.DataSource = dtPisosHotelAct;
+            comboBoxNuevoPiso.DisplayMember = "piso";
+            comboBoxNuevoPiso.ValueMember = "piso";
+        }
+
+        private void VisibilizarComponentes()
+        {
+            labelNuevosCambios.Visible = true;
+            labelDescripcionNueva.Visible = true;
+            labelHotelNuevo.Visible = true;
+            labelPisoNuevo.Visible = true;
+            comboBoxNuevoHotel.Visible = true;
+            comboBoxNuevoPiso.Visible = true;
+            checkBoxEstaHabilitado.Visible = true;
+            checkBoxTieneVistaExterior.Visible = true;
+            textBoxNuevaDescripcion.Visible = true;
         }
 
         private void comboBoxHoteles_SelectedIndexChanged(object sender, EventArgs e)
@@ -50,5 +100,14 @@ namespace FrbaHotel.AbmHabitacion
             comboBoxNumeroHabitacion.DisplayMember = "numero";
             comboBoxNumeroHabitacion.ValueMember = "numero";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConexionSQL c = new ConexionSQL();
+            c.cargarTablaSQL("update WHERE_EN_EL_DELETE_FROM.habitaciones set hoteles_id="+comboBoxNuevoHotel.SelectedValue+" descripcion="+textBoxNuevaDescripcion.Text+
+                " frente="+checkBoxTieneVistaExterior.Checked+" habilitado=" + checkBoxEstaHabilitado.Checked + " where habitacion_id=" + comboBoxNumeroHabitacion.SelectedValue);
+        }
+
+    
     }
 }
