a75b9578176e0f7c307c2fd0131aecffcee53b45
Brenda Stolarz <brenda.stolarz@gmail.com>
2018-06-25 00:27:44 -0300

    Reservas: Desdoblo las 2 funcionalidades

---
+++ FrbaHotel/GenerarModificacionReserva/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FrbaHotel.GenerarModificacionReserva
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnModificarCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar nueva reserva";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnModificarCancelar
+            // 
+            this.btnModificarCancelar.Location = new System.Drawing.Point(281, 145);
+            this.btnModificarCancelar.Name = "btnModificarCancelar";
+            this.btnModificarCancelar.Size = new System.Drawing.Size(140, 26);
+            this.btnModificarCancelar.TabIndex = 1;
+            this.btnModificarCancelar.Text = "Modificar/borrar reserva";
+            this.btnModificarCancelar.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 273);
+            this.Controls.Add(this.btnModificarCancelar);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnModificarCancelar;
+    }
+}
