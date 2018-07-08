e24a5757f0f84b503726580840a12906d40c6759
juanmacaru <juanmanuelcarucci@gmail.com>
2018-07-08 20:36:43 -0300

    arreglos en check in
    sumo funcionalidad de checkin

--- FrbaHotel/RegistrarEstadia/CheckIn.Designer.cs
+++ FrbaHotel/RegistrarEstadia/CheckIn.Designer.cs
@@ -28,81 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.nroreserva = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.fechaingreso = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dgvReservas = new System.Windows.Forms.DataGridView();
+            this.chkSeleccionar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Aceptar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservas)).BeginInit();
             this.SuspendLayout();
             // 
-            // nroreserva
+            // groupBox1
             // 
-            this.nroreserva.Location = new System.Drawing.Point(197, 54);
-            this.nroreserva.Name = "nroreserva";
-            this.nroreserva.Size = new System.Drawing.Size(200, 20);
-            this.nroreserva.TabIndex = 0;
-            this.nroreserva.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(23, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(401, 50);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Reserva";
             // 
-            // label1
+            // txtCodigo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 54);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Numero de reserva:";
+            this.txtCodigo.Location = new System.Drawing.Point(100, 19);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(71, 20);
+            this.txtCodigo.TabIndex = 1;
             // 
-            // button1
+            // label3
             // 
-            this.button1.Location = new System.Drawing.Point(171, 167);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Aceptar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ingrese codigo: ";
             // 
-            // label2
+            // btnBuscar
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 102);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Fecha de Ingreso:";
+            this.btnBuscar.Location = new System.Drawing.Point(23, 85);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(105, 21);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
-            // fechaingreso
+            // dgvReservas
             // 
-            this.fechaingreso.Location = new System.Drawing.Point(197, 102);
-            this.fechaingreso.Name = "fechaingreso";
-            this.fechaingreso.Size = new System.Drawing.Size(200, 20);
-            this.fechaingreso.TabIndex = 4;
-            this.fechaingreso.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.dgvReservas.AllowUserToAddRows = false;
+            this.dgvReservas.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dgvReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReservas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.chkSeleccionar});
+            this.dgvReservas.Location = new System.Drawing.Point(23, 112);
+            this.dgvReservas.Name = "dgvReservas";
+            this.dgvReservas.Size = new System.Drawing.Size(397, 64);
+            this.dgvReservas.TabIndex = 6;
+            // 
+            // chkSeleccionar
+            // 
+            this.chkSeleccionar.HeaderText = "Seleccionar";
+            this.chkSeleccionar.Name = "chkSeleccionar";
+            // 
+            // Aceptar
+            // 
+            this.Aceptar.Location = new System.Drawing.Point(172, 188);
+            this.Aceptar.Name = "Aceptar";
+            this.Aceptar.Size = new System.Drawing.Size(75, 23);
+            this.Aceptar.TabIndex = 7;
+            this.Aceptar.Text = "Aceptar";
+            this.Aceptar.UseVisualStyleBackColor = true;
+            this.Aceptar.Click += new System.EventHandler(this.Aceptar_Click);
             // 
             // CheckIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(436, 223);
-            this.Controls.Add(this.fechaingreso);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.nroreserva);
+            this.Controls.Add(this.Aceptar);
+            this.Controls.Add(this.dgvReservas);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.groupBox1);
             this.Name = "CheckIn";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.CheckIn_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservas)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox nroreserva;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker fechaingreso;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dgvReservas;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn chkSeleccionar;
+        private System.Windows.Forms.Button Aceptar;
     }
 }
