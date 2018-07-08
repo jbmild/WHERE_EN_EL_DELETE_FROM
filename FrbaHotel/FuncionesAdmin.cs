8c2ac772f93225fd287cbd28f11ee2585ffbf5ea
jbmild <jbmild@gmail.com>
2018-07-08 17:24:57 -0300

    agrego funcionalidad del boton de reservas

--- FrbaHotel/FuncionesAdmin.cs
+++ FrbaHotel/FuncionesAdmin.cs
@@ -52,7 +52,7 @@ namespace FrbaHotel
             {
                 switch ( p.PermisoId) {
 
-                    case 1: btn1.Visible = true;
+                    case 1: btnRoles.Visible = true;
                         break;
                     case 2: btn2.Visible = true;
                         break;
@@ -79,6 +79,12 @@ namespace FrbaHotel
                 }
             }
                
+        }
+
+        private void btnRoles_Click(object sender, EventArgs e)
+        {
+            Roles.frmRolesListado frmListado = new Roles.frmRolesListado();
+            frmListado.Show();
         }
     }
 }
