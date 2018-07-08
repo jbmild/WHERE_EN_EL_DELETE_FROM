adc2d3d746402e590b05eb556b9d32e77885a458
Brenda Stolarz <brenda.stolarz@gmail.com>
2018-07-08 12:32:54 -0300

    script: agregado de permisos al rol admin

--- data/sp_obtenerHabitacionesDisponibles.sql
+++ data/sp_obtenerHabitacionesDisponibles.sql
@@ -17,7 +17,7 @@ GO
 
 -- WHERE_EN_EL_DELETE_FROM.obtenerHabitacionesDisponibles @fdesde='02/06/2018 03:31:02 p.m.',@fhasta='02/06/2018 03:31:02 p.m.',@hotel_id=1,@regimen_id=1,@tipoHabitacion_id=1
 
-
+/*
 ALTER PROCEDURE [WHERE_EN_EL_DELETE_FROM].[obtenerHabitacionesDisponibles]
 	@fdesde varchar(30),
 	@fhasta varchar(30),
@@ -26,8 +26,9 @@ ALTER PROCEDURE [WHERE_EN_EL_DELETE_FROM].[obtenerHabitacionesDisponibles]
 	@tipoHabitacion_id int
 
 AS
+*/
 BEGIN
-		/*
+		
 		DECLARE @fdesde varchar(30),
 		@fhasta varchar(30),
 		@hotel_id int,
@@ -35,74 +36,74 @@ BEGIN
 		@tipoHabitacion_id int
 
 
-		SELECT @fdesde = '01/06/2016 03:31:02 p.m.',
-		@fhasta = '24/06/2016 03:31:02 p.m.',
+		SELECT @fdesde = '01/07/2018 12:00:00 a.m.',
+		@fhasta = '03/07/2018 12:00:00 a.m.',
 		@hotel_id = 1,
-		@regimen_id = NULL,
-		@tipoHabitacion_id = 1
-		*/
+		@regimen_id = 1,
+		@tipoHabitacion_id = 4
+		
 	
 	
 	DECLARE @FechaDesde datetime
-	DECLARE @FechaHasta datetime
+	                                DECLARE @FechaHasta datetime
 	
-	SELECT @FechaDesde = convert(datetime, SUBSTRING(@fdesde, 0, 11), 103)
-	SELECT @FechaHasta = convert(datetime, SUBSTRING(@fhasta, 0, 11) + ' 23:59:59', 103)
+	                                SELECT @FechaDesde = convert(datetime, SUBSTRING(@fdesde, 0, 11), 103)
+	                                SELECT @FechaHasta = convert(datetime, SUBSTRING(@fhasta, 0, 11) + ' 23:59:59', 103)
 
-	SET NOCOUNT ON;
+	                                SET NOCOUNT ON;
 
-	SELECT distinct
-		hab.numero AS [Nro Habitacion],
-		hot.direccion AS Hotel,
-		hot.hotel_id,
-		hab.habitacion_id,
-		habTipos.max_huespedes AS [Cant Huespedes],
-		reg.precio * habTipos.max_huespedes * hot.estrellas_recargo AS precio,
-		reg.descripcion AS TipoRegimen,
-		reg.codigo AS CodigoRegimen
-	FROM
-		WHERE_EN_EL_DELETE_FROM.habitaciones hab
-	INNER JOIN
-		WHERE_EN_EL_DELETE_FROM.hoteles hot on
-		hot.hotel_id = hab.hotel_id
-		and (hab.hotel_id = @hotel_id or @hotel_id is null)
-		and (hab.tipos_id = @tipoHabitacion_id or @tipoHabitacion_id is null)
-	INNER JOIN
-		WHERE_EN_EL_DELETE_FROM.regimenes_hoteles regHot on
-		regHot.hotel_id = hot.hotel_id
-		and (regHot.regimen_id = @regimen_id or @regimen_id is null)
-	INNER JOIN
-		WHERE_EN_EL_DELETE_FROM.regimenes reg on
-		reg.regimen_id = regHot.regimen_id
-	INNER JOIN
-		WHERE_EN_EL_DELETE_FROM.habitaciones_tipos habTipos on
-		habTipos.tipo_id = hab.tipos_id
-	LEFT JOIN(
-		SELECT
-			res.reserva_id,
-			resHab.habitacion_id
-		FROM
-			WHERE_EN_EL_DELETE_FROM.reservas_habitaciones resHab
-		INNER JOIN
-			WHERE_EN_EL_DELETE_FROM.reservas res on
-			res.reserva_id = resHab.reserva_id
-			and res.estado not in ('cancelada_recepcion', 'cancelada_cliente', 'cancelada_noshow', 'efectivizada')
-			and (
-					@FechaDesde is null 
-					OR 
-					@FechaHasta is null
-					OR
-					@FechaDesde between res.fecha_desde and res.fecha_hasta
-					OR
-					@FechaDesde between res.fecha_desde and res.fecha_hasta
-					OR
-					(@FechaDesde < res.fecha_desde and @FechaHasta > res.fecha_hasta)
-				)
-		) res on res.habitacion_id = hab.habitacion_id
+	                                SELECT distinct
+		                                hab.numero AS [Nro Habitacion],
+		                                hot.direccion AS Hotel,
+		                                hot.hotel_id,
+		                                hab.habitacion_id,
+		                                habTipos.max_huespedes AS [Cant Huespedes],
+		                                reg.precio * habTipos.max_huespedes * hot.estrellas_recargo AS precio,
+		                                reg.descripcion AS TipoRegimen,
+		                                reg.codigo AS CodigoRegimen
+	                                FROM
+		                                WHERE_EN_EL_DELETE_FROM.habitaciones hab
+	                                INNER JOIN
+		                                WHERE_EN_EL_DELETE_FROM.hoteles hot on
+		                                hot.hotel_id = hab.hotel_id
+		                                and (hab.hotel_id = @hotel_id or @hotel_id is null)
+		                                and (hab.tipos_id = @tipoHabitacion_id or @tipoHabitacion_id is null or @tipoHabitacion_id = -1)
+	                                INNER JOIN
+		                                WHERE_EN_EL_DELETE_FROM.regimenes_hoteles regHot on
+		                                regHot.hotel_id = hot.hotel_id
+		                                and (regHot.regimen_id = @regimen_id or @regimen_id is null)
+	                                INNER JOIN
+		                                WHERE_EN_EL_DELETE_FROM.regimenes reg on
+		                                reg.regimen_id = regHot.regimen_id
+	                                INNER JOIN
+		                                WHERE_EN_EL_DELETE_FROM.habitaciones_tipos habTipos on
+		                                habTipos.tipo_id = hab.tipos_id
+	                                LEFT JOIN(
+		                                SELECT
+			                                res.reserva_id,
+			                                resHab.habitacion_id
+		                                FROM
+			                                WHERE_EN_EL_DELETE_FROM.reservas_habitaciones resHab
+		                                INNER JOIN
+			                                WHERE_EN_EL_DELETE_FROM.reservas res on
+			                                res.reserva_id = resHab.reserva_id
+			                                and res.estado not in ('cancelada_recepcion', 'cancelada_cliente', 'cancelada_noshow', 'efectivizada')
+			                                and (
+					                                @FechaDesde is null 
+					                                OR 
+					                                @FechaHasta is null
+					                                OR
+					                                @FechaDesde between res.fecha_desde and res.fecha_hasta
+					                                OR
+					                                @FechaDesde between res.fecha_desde and res.fecha_hasta
+					                                OR
+					                                (@FechaDesde < res.fecha_desde and @FechaHasta > res.fecha_hasta)
+				                                )
+		                                ) res on res.habitacion_id = hab.habitacion_id
 		
-	WHERE
-		res.reserva_id is null 
-
+	                                WHERE
+		                                res.reserva_id is null
+                                        
 
 	
 	
