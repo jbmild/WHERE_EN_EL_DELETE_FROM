2d3df78ede169c906bfc17896225a7d2ecece740
jbmild <jbmild@gmail.com>
2018-07-08 20:45:39 -0300

    cambios de formulario

---
+++ FrbaHotel/Bienvenido.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace FrbaHotel
+{
+    partial class Bienvenido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.lblHotel = new System.Windows.Forms.Label();
+            this.lblRol = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.btnCancelarReserva = new System.Windows.Forms.Button();
+            this.btnGenerarModificarReserva = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnRoles = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.btnHoteles = new System.Windows.Forms.Button();
+            this.btnHabitaciones = new System.Windows.Forms.Button();
+            this.btnEstadias = new System.Windows.Forms.Button();
+            this.btnConsumibles = new System.Windows.Forms.Button();
+            this.btnFacturacion = new System.Windows.Forms.Button();
+            this.btnEstadisticas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenido.Location = new System.Drawing.Point(299, 30);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(584, 32);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "Bienvenido al sistema de gestion hotelera";
+            // 
+            // lblHotel
+            // 
+            this.lblHotel.AutoSize = true;
+            this.lblHotel.Location = new System.Drawing.Point(12, 9);
+            this.lblHotel.Name = "lblHotel";
+            this.lblHotel.Size = new System.Drawing.Size(51, 20);
+            this.lblHotel.TabIndex = 1;
+            this.lblHotel.Text = "Hotel:";
+            // 
+            // lblRol
+            // 
+            this.lblRol.AutoSize = true;
+            this.lblRol.Location = new System.Drawing.Point(12, 30);
+            this.lblRol.Name = "lblRol";
+            this.lblRol.Size = new System.Drawing.Size(37, 20);
+            this.lblRol.TabIndex = 2;
+            this.lblRol.Text = "Rol:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(1032, 32);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(116, 38);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(1060, 7);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(64, 20);
+            this.lblUsuario.TabIndex = 4;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // btnCancelarReserva
+            // 
+            this.btnCancelarReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelarReserva.Location = new System.Drawing.Point(641, 115);
+            this.btnCancelarReserva.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancelarReserva.Name = "btnCancelarReserva";
+            this.btnCancelarReserva.Size = new System.Drawing.Size(208, 57);
+            this.btnCancelarReserva.TabIndex = 17;
+            this.btnCancelarReserva.Text = "Cancelar Reserva";
+            this.btnCancelarReserva.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerarModificarReserva
+            // 
+            this.btnGenerarModificarReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarModificarReserva.Location = new System.Drawing.Point(333, 115);
+            this.btnGenerarModificarReserva.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGenerarModificarReserva.Name = "btnGenerarModificarReserva";
+            this.btnGenerarModificarReserva.Size = new System.Drawing.Size(300, 57);
+            this.btnGenerarModificarReserva.TabIndex = 16;
+            this.btnGenerarModificarReserva.Text = "Generar o modificar Reserva";
+            this.btnGenerarModificarReserva.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.Location = new System.Drawing.Point(189, 182);
+            this.btnClientes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(195, 57);
+            this.btnClientes.TabIndex = 18;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // btnRoles
+            // 
+            this.btnRoles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoles.Location = new System.Drawing.Point(883, 249);
+            this.btnRoles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRoles.Name = "btnRoles";
+            this.btnRoles.Size = new System.Drawing.Size(184, 57);
+            this.btnRoles.TabIndex = 19;
+            this.btnRoles.Text = "Roles";
+            this.btnRoles.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Location = new System.Drawing.Point(307, 249);
+            this.btnUsuarios.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(184, 57);
+            this.btnUsuarios.TabIndex = 20;
+            this.btnUsuarios.Text = "Usuarios";
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // btnHoteles
+            // 
+            this.btnHoteles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoteles.Location = new System.Drawing.Point(115, 249);
+            this.btnHoteles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnHoteles.Name = "btnHoteles";
+            this.btnHoteles.Size = new System.Drawing.Size(184, 57);
+            this.btnHoteles.TabIndex = 21;
+            this.btnHoteles.Text = "Hoteles";
+            this.btnHoteles.UseVisualStyleBackColor = true;
+            // 
+            // btnHabitaciones
+            // 
+            this.btnHabitaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHabitaciones.Location = new System.Drawing.Point(499, 249);
+            this.btnHabitaciones.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnHabitaciones.Name = "btnHabitaciones";
+            this.btnHabitaciones.Size = new System.Drawing.Size(184, 57);
+            this.btnHabitaciones.TabIndex = 22;
+            this.btnHabitaciones.Text = "Habitaciones";
+            this.btnHabitaciones.UseVisualStyleBackColor = true;
+            // 
+            // btnEstadias
+            // 
+            this.btnEstadias.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstadias.Location = new System.Drawing.Point(798, 182);
+            this.btnEstadias.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEstadias.Name = "btnEstadias";
+            this.btnEstadias.Size = new System.Drawing.Size(195, 57);
+            this.btnEstadias.TabIndex = 23;
+            this.btnEstadias.Text = "Estadias";
+            this.btnEstadias.UseVisualStyleBackColor = true;
+            // 
+            // btnConsumibles
+            // 
+            this.btnConsumibles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsumibles.Location = new System.Drawing.Point(392, 182);
+            this.btnConsumibles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConsumibles.Name = "btnConsumibles";
+            this.btnConsumibles.Size = new System.Drawing.Size(195, 57);
+            this.btnConsumibles.TabIndex = 24;
+            this.btnConsumibles.Text = "Consumibles";
+            this.btnConsumibles.UseVisualStyleBackColor = true;
+            // 
+            // btnFacturacion
+            // 
+            this.btnFacturacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFacturacion.Location = new System.Drawing.Point(595, 182);
+            this.btnFacturacion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFacturacion.Name = "btnFacturacion";
+            this.btnFacturacion.Size = new System.Drawing.Size(195, 57);
+            this.btnFacturacion.TabIndex = 25;
+            this.btnFacturacion.Text = "Facturacion";
+            this.btnFacturacion.UseVisualStyleBackColor = true;
+            // 
+            // btnEstadisticas
+            // 
+            this.btnEstadisticas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstadisticas.Location = new System.Drawing.Point(691, 249);
+            this.btnEstadisticas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEstadisticas.Name = "btnEstadisticas";
+            this.btnEstadisticas.Size = new System.Drawing.Size(184, 57);
+            this.btnEstadisticas.TabIndex = 26;
+            this.btnEstadisticas.Text = "Estadisticas";
+            this.btnEstadisticas.UseVisualStyleBackColor = true;
+            // 
+            // Bienvenido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 379);
+            this.Controls.Add(this.btnEstadisticas);
+            this.Controls.Add(this.btnFacturacion);
+            this.Controls.Add(this.btnConsumibles);
+            this.Controls.Add(this.btnEstadias);
+            this.Controls.Add(this.btnHabitaciones);
+            this.Controls.Add(this.btnHoteles);
+            this.Controls.Add(this.btnUsuarios);
+            this.Controls.Add(this.btnRoles);
+            this.Controls.Add(this.btnClientes);
+            this.Controls.Add(this.btnCancelarReserva);
+            this.Controls.Add(this.btnGenerarModificarReserva);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblRol);
+            this.Controls.Add(this.lblHotel);
+            this.Controls.Add(this.lblBienvenido);
+            this.Name = "Bienvenido";
+            this.Text = "Bienvenido";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.Label lblHotel;
+        private System.Windows.Forms.Label lblRol;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Button btnCancelarReserva;
+        private System.Windows.Forms.Button btnGenerarModificarReserva;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnRoles;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Button btnHoteles;
+        private System.Windows.Forms.Button btnHabitaciones;
+        private System.Windows.Forms.Button btnEstadias;
+        private System.Windows.Forms.Button btnConsumibles;
+        private System.Windows.Forms.Button btnFacturacion;
+        private System.Windows.Forms.Button btnEstadisticas;
+    }
+}
---
+++ FrbaHotel/Tools/Sesion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrbaHotel.Modelo;
+using FrbaHotel.Roles.Modelo;
+
+namespace FrbaHotel.Tools
+{
+    public static class Sesion
+    {
+        public static Usuario usuario = null;
+        public static Rol rol = null;
+        public static Hotel hotel = null;
+    }
+}
