2ae393956f7b5367ccae114512b54b26cab8ba86
Brenda Stolarz <brenda.stolarz@gmail.com>
2018-07-08 00:02:11 -0300

    listado clientes first commit

---
+++ FrbaHotel/AbmCliente/frmClientesListado.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace FrbaHotel.AbmCliente
+{
+    partial class frmClientesListado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwRoles = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblHabilitado = new System.Windows.Forms.Label();
+            this.cmbHabilitado = new System.Windows.Forms.ComboBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnAlta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwRoles)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgwRoles
+            // 
+            this.dgwRoles.AllowUserToAddRows = false;
+            this.dgwRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwRoles.Location = new System.Drawing.Point(8, 112);
+            this.dgwRoles.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgwRoles.MultiSelect = false;
+            this.dgwRoles.Name = "dgwRoles";
+            this.dgwRoles.RowTemplate.Height = 28;
+            this.dgwRoles.Size = new System.Drawing.Size(479, 226);
+            this.dgwRoles.TabIndex = 0;
+            this.dgwRoles.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgwRoles_CellMouseClick);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(61, 19);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(178, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(4, 21);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblHabilitado);
+            this.groupBox1.Controls.Add(this.cmbHabilitado);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.lblNombre);
+            this.groupBox1.Location = new System.Drawing.Point(8, 8);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(479, 72);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros";
+            // 
+            // lblHabilitado
+            // 
+            this.lblHabilitado.AutoSize = true;
+            this.lblHabilitado.Location = new System.Drawing.Point(4, 45);
+            this.lblHabilitado.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHabilitado.Name = "lblHabilitado";
+            this.lblHabilitado.Size = new System.Drawing.Size(54, 13);
+            this.lblHabilitado.TabIndex = 5;
+            this.lblHabilitado.Text = "Habilitado";
+            // 
+            // cmbHabilitado
+            // 
+            this.cmbHabilitado.Items.AddRange(new object[] {
+            " - ",
+            "No",
+            "Si"});
+            this.cmbHabilitado.Location = new System.Drawing.Point(61, 44);
+            this.cmbHabilitado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbHabilitado.Name = "cmbHabilitado";
+            this.cmbHabilitado.Size = new System.Drawing.Size(178, 21);
+            this.cmbHabilitado.TabIndex = 4;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(8, 84);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(81, 25);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(406, 84);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(81, 25);
+            this.btnBuscar.TabIndex = 7;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnAlta
+            // 
+            this.btnAlta.Location = new System.Drawing.Point(406, 343);
+            this.btnAlta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAlta.Name = "btnAlta";
+            this.btnAlta.Size = new System.Drawing.Size(81, 25);
+            this.btnAlta.TabIndex = 8;
+            this.btnAlta.Text = "Alta";
+            this.btnAlta.UseVisualStyleBackColor = true;
+            this.btnAlta.Click += new System.EventHandler(this.btnAlta_Click);
+            // 
+            // frmClientesListado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 376);
+            this.Controls.Add(this.btnAlta);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgwRoles);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmClientesListado";
+            this.Text = "Administracion de Clientes";
+            this.Load += new System.EventHandler(this.frmRolesGrid_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwRoles)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwRoles;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblHabilitado;
+        private System.Windows.Forms.ComboBox cmbHabilitado;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnAlta;
+    }
+}
---
+++ FrbaHotel/AbmCliente/frmClientesListado.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FrbaHotel.Modelo;
+
+namespace FrbaHotel.AbmCliente
+{
+    public partial class frmClientesListado : Form
+    {
+        public frmClientesListado()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRolesGrid_Load(object sender, EventArgs e)
+        {
+            this.cmbHabilitado.SelectedIndex = 2;
+            //this.LoadGrid(Modelo.Roles.obtener(txtNombre.Text, cmbHabilitado.SelectedIndex - 1));
+            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtNombre.Text = "";
+            this.cmbHabilitado.SelectedIndex = 2;
+            this.btnBuscar_Click(sender, e);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //this.LoadGrid(Modelo.Roles.obtener(txtNombre.Text, cmbHabilitado.SelectedIndex-1));
+        }
+
+        public void LoadGrid(DataTable dt)
+        {
+            this.dgwRoles.DataSource = null;
+            using (dt)
+            {
+                this.dgwRoles.AutoGenerateColumns = false;
+                this.dgwRoles.Columns.Clear();
+
+                DataGridViewTextBoxColumn id = new DataGridViewTextBoxColumn();
+                id.HeaderText = "Id";
+                id.DataPropertyName = "rol_id";
+                id.ReadOnly = true;
+                id.Visible = false;
+                this.dgwRoles.Columns.Add(id);
+
+                DataGridViewTextBoxColumn nombre = new DataGridViewTextBoxColumn();
+                nombre.HeaderText = "Nombre";
+                nombre.DataPropertyName = "nombre";
+                nombre.ReadOnly = true;
+                this.dgwRoles.Columns.Add(nombre);
+
+                DataGridViewCheckBoxColumn habilitado = new DataGridViewCheckBoxColumn();
+                habilitado.HeaderText = "Habilitado";
+                habilitado.DataPropertyName = "habilitado";
+                habilitado.ReadOnly = true;
+                habilitado.FalseValue = "0";
+                habilitado.TrueValue = "1";
+                this.dgwRoles.Columns.Add(habilitado);
+
+                DataGridViewTextBoxColumn modificar = new DataGridViewTextBoxColumn();
+                modificar.HeaderText = "Modificar";
+                modificar.ReadOnly = true;
+                this.dgwRoles.Columns.Add(modificar);
+
+                DataGridViewTextBoxColumn eliminar = new DataGridViewTextBoxColumn();
+                eliminar.HeaderText = "Eliminar";
+                eliminar.ReadOnly = true;
+                eliminar.DefaultCellStyle.ForeColor = Color.Aqua;
+                this.dgwRoles.Columns.Add(eliminar);
+
+                this.dgwRoles.DataSource = dt;
+            }
+        }
+
+        private void dgwRoles_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                switch (this.dgwRoles.CurrentCell.ColumnIndex)
+                {
+                    case 3:
+                        this.modificarRol((int)this.dgwRoles.Rows[e.RowIndex].Cells[0].Value);
+                        break;
+                    case 4:
+                        this.deleteRol((int)this.dgwRoles.Rows[e.RowIndex].Cells[0].Value, this.dgwRoles.Rows[e.RowIndex].Cells[1].Value.ToString());
+                        break;
+
+                }
+            }
+        }
+
+        private void modificarRol(int rolId)
+        {
+            /*
+            Rol rol = new Rol(rolId);
+            Roles.frmRolesFicha frmFicha = new Roles.frmRolesFicha(rol);
+            frmFicha.FormClosed += new FormClosedEventHandler(frmRolesFicha_Closed);
+            frmFicha.Show();
+             * */
+        }
+
+        private void deleteRol(int rolId, string name)
+        {
+            /*
+            var confirmResult = MessageBox.Show(String.Concat("Esta seguro que desea eliminar logicamente el rol '", name, "'?"), "Confirmar eliminacion logica", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                Rol rol = new Rol(rolId);
+                List<KeyValuePair<string,string>> errores = rol.eliminar();
+                if (errores.Count == 0)
+                {
+                    this.LoadGrid(Modelo.Roles.obtener(txtNombre.Text, cmbHabilitado.SelectedIndex - 1));
+                    MessageBox.Show("Se realizo el eliminado logico exitosamente!");
+                }
+                else
+                {
+                    string impresionErrores = "";
+                    foreach(KeyValuePair<string, string> data in errores){
+                        if(impresionErrores.Length==0){
+                            impresionErrores = data.Value;
+                        }else{
+                            impresionErrores = impresionErrores + Environment.NewLine + data.Value;
+                        }
+                    }
+
+                    MessageBox.Show(impresionErrores, "Ocurrio un error al eliminar el rol.");
+                }
+                 
+            }
+             * */
+
+        }
+
+        private void btnAlta_Click(object sender, EventArgs e)
+        {
+            /*Rol rol = new Rol(0);
+            Roles.frmRolesFicha frmFicha = new Roles.frmRolesFicha(rol);
+            frmFicha.FormClosed += new FormClosedEventHandler(frmRolesFicha_Closed);
+            frmFicha.Show();
+            */             
+        }
+
+
+        private void frmRolesFicha_Closed(object sender, FormClosedEventArgs e)
+        {
+            //this.LoadGrid(Modelo.Roles.obtener(txtNombre.Text, cmbHabilitado.SelectedIndex - 1));
+        }
+    }
+}
