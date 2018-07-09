3c9b5a8631e561e4099cb19197b746bcda74af0b
juanmacaru <juanmanuelcarucci@gmail.com>
2018-07-09 20:01:53 -0300

    avances en check-in
    avances en check-in.

---
+++ FrbaHotel/RegistrarEstadia/PreguntaNuevaRerva.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaHotel.RegistrarEstadia
+{
+    public partial class PreguntaNuevaRerva : Form
+    {
+        public PreguntaNuevaRerva()
+        {
+            InitializeComponent();
+        }
+
+        private void PreguntaNuevaRerva_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Si
+            GenerarModificacionReserva.GenerarReservaPrincipal generarReservaPrincipal = new GenerarModificacionReserva.GenerarReservaPrincipal();
+            this.Hide();
+            generarReservaPrincipal.Show();            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //No
+            FuncionesAdmin funcionesAdmin = new FuncionesAdmin();
+            this.Hide();
+            funcionesAdmin.Show();
+        }
+    }
+}
