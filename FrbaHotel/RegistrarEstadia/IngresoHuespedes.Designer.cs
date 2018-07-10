c92da279a4394c1cf91400e584e77fdfc4e92747
juanmacaru <juanmanuelcarucci@gmail.com>
2018-07-09 22:37:29 -0300

    avances en checkin

---
+++ FrbaHotel/RegistrarEstadia/IngresoHuespedes.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace FrbaHotel.RegistrarEstadia
+{
+    partial class IngresoHuespedes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNroDoc = new System.Windows.Forms.TextBox();
+            this.cmbTipoDoc = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnAlta = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgwRoles = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwRoles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtNroDoc);
+            this.groupBox1.Controls.Add(this.cmbTipoDoc);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtApellido);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.lblNombre);
+            this.groupBox1.Location = new System.Drawing.Point(11, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(500, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(385, 19);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(94, 20);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // txtNroDoc
+            // 
+            this.txtNroDoc.Location = new System.Drawing.Point(269, 52);
+            this.txtNroDoc.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNroDoc.Name = "txtNroDoc";
+            this.txtNroDoc.Size = new System.Drawing.Size(68, 20);
+            this.txtNroDoc.TabIndex = 12;
+            // 
+            // cmbTipoDoc
+            // 
+            this.cmbTipoDoc.FormattingEnabled = true;
+            this.cmbTipoDoc.Location = new System.Drawing.Point(267, 21);
+            this.cmbTipoDoc.Name = "cmbTipoDoc";
+            this.cmbTipoDoc.Size = new System.Drawing.Size(70, 21);
+            this.cmbTipoDoc.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(352, 21);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Email: ";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(191, 46);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nro de identificación: ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(191, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Tipo de identificación: ";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(61, 41);
+            this.txtApellido.Margin = new System.Windows.Forms.Padding(2);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(94, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Apellido: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(61, 19);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(94, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(4, 21);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // btnAlta
+            // 
+            this.btnAlta.Location = new System.Drawing.Point(430, 348);
+            this.btnAlta.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAlta.Name = "btnAlta";
+            this.btnAlta.Size = new System.Drawing.Size(81, 25);
+            this.btnAlta.TabIndex = 12;
+            this.btnAlta.Text = "Alta";
+            this.btnAlta.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(430, 118);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(81, 25);
+            this.btnBuscar.TabIndex = 11;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(11, 118);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(81, 25);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // dgwRoles
+            // 
+            this.dgwRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwRoles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dgwRoles.Enabled = false;
+            this.dgwRoles.Location = new System.Drawing.Point(11, 147);
+            this.dgwRoles.Margin = new System.Windows.Forms.Padding(2);
+            this.dgwRoles.Name = "dgwRoles";
+            this.dgwRoles.ReadOnly = true;
+            this.dgwRoles.RowTemplate.Height = 24;
+            this.dgwRoles.Size = new System.Drawing.Size(500, 197);
+            this.dgwRoles.TabIndex = 13;
+            this.dgwRoles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.NullValue = "Seleccionar";
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column1.Text = "Seleccionar";
+            // 
+            // IngresoHuespedes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 384);
+            this.Controls.Add(this.dgwRoles);
+            this.Controls.Add(this.btnAlta);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "IngresoHuespedes";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwRoles)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNroDoc;
+        private System.Windows.Forms.ComboBox cmbTipoDoc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnAlta;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dgwRoles;
+        private System.Windows.Forms.DataGridViewButtonColumn Column1;
+    }
+}
---
+++ FrbaHotel/RegistrarEstadia/IngresoHuespedes.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FrbaHotel.Modelo;
+
+namespace FrbaHotel.RegistrarEstadia
+{
+    public partial class IngresoHuespedes : Form
+    {
+        public IngresoHuespedes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Cliente cli = new Cliente();
+            this.LoadGrid(cli.getClientes(cmbTipoDoc.Text, txtNroDoc.Text, txtEmail.Text, txtNombre.Text, txtApellido.Text));
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void LoadGrid(DataTable dt)
+        {
+            this.dgwRoles.DataSource = null;
+            using (dt)
+            {
+                this.dgwRoles.AutoGenerateColumns = false;
+                this.dgwRoles.Columns.Clear();
+
+                DataGridViewTextBoxColumn id = new DataGridViewTextBoxColumn();
+                id.HeaderText = "ID";
+                id.DataPropertyName = "cliente_id";
+                id.Visible = false;
+                this.dgwRoles.Columns.Add(id);
+
+                DataGridViewTextBoxColumn habilitado = new DataGridViewTextBoxColumn();
+                habilitado.HeaderText = "Habilitado";
+                habilitado.DataPropertyName = "EstaHabilitado";
+                habilitado.ReadOnly = true;
+                this.dgwRoles.Columns.Add(habilitado);
+
+                DataGridViewTextBoxColumn nombre = new DataGridViewTextBoxColumn();
+                nombre.HeaderText = "Nombre";
+                nombre.DataPropertyName = "nombre";
+                nombre.ReadOnly = true;
+                this.dgwRoles.Columns.Add(nombre);
+
+                DataGridViewTextBoxColumn apellido = new DataGridViewTextBoxColumn();
+                apellido.HeaderText = "Apellido";
+                apellido.DataPropertyName = "apellido";
+                apellido.ReadOnly = true;
+                this.dgwRoles.Columns.Add(apellido);
+
+                DataGridViewTextBoxColumn tipoDoc = new DataGridViewTextBoxColumn();
+                tipoDoc.HeaderText = "Tipo Identificación";
+                tipoDoc.DataPropertyName = "documento_tipo";
+                tipoDoc.ReadOnly = true;
+                this.dgwRoles.Columns.Add(tipoDoc);
+
+                DataGridViewTextBoxColumn nroDoc = new DataGridViewTextBoxColumn();
+                nroDoc.HeaderText = "Nro Identificación";
+                nroDoc.DataPropertyName = "documento_nro";
+                nroDoc.ReadOnly = true;
+                this.dgwRoles.Columns.Add(nroDoc);
+
+                DataGridViewTextBoxColumn email = new DataGridViewTextBoxColumn();
+                email.HeaderText = "Email";
+                email.DataPropertyName = "mail";
+                email.ReadOnly = true;
+                this.dgwRoles.Columns.Add(email);
+
+                DataGridViewTextBoxColumn modificar = new DataGridViewTextBoxColumn();
+                modificar.HeaderText = "Modificar";
+                modificar.ReadOnly = true;
+                this.dgwRoles.Columns.Add(modificar);
+
+                DataGridViewTextBoxColumn eliminar = new DataGridViewTextBoxColumn();
+                eliminar.HeaderText = "Eliminar";
+                eliminar.ReadOnly = true;
+                eliminar.DefaultCellStyle.ForeColor = Color.Aqua;
+                this.dgwRoles.Columns.Add(eliminar);
+
+                this.dgwRoles.DataSource = dt;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex.Equals(0))
+            {
+                //int row = e.RowIndex;
+                //int tieneVista = 0;
+                //int estaHabilitado = 0;
+
+                //DataGridViewRow selectedRow = dgwRoles.Rows[row];
+                //int piso = this.mapearPiso(selectedRow);
+                //HabitacionElegida habitacion = new HabitacionElegida();
+                ////System.Windows.Forms.MessageBox.Show(dataGridView1.Columns.Count.ToString());
+
+
+                //habitacion.SetNumero(Int32.Parse(selectedRow.Cells[1].Value.ToString()));
+                //if (piso.Equals(0))
+                //{
+                //    habitacion.SetPiso(Int32.Parse(selectedRow.Cells[2].Value.ToString()));
+                //}
+                //else
+                //{
+                //    habitacion.SetPiso(piso);
+                //}
+
+                //habitacion.SetDescripcion(selectedRow.Cells[3].Value.ToString());
+                //habitacion.SetTipo(selectedRow.Cells[4].Value.ToString());
+                //habitacion.SetDireccion(selectedRow.Cells[7].Value.ToString());
+                //habitacion.SetHabiID(Int32.Parse(selectedRow.Cells[8].Value.ToString()));
+                //habitacion.SetHotelID(this.ObtenerHotelID(habitacion));
+
+
+                //if (selectedRow.Cells[5].Value.Equals(true))
+                //{
+                //    tieneVista = 1;
+                //}
+                //if (selectedRow.Cells[6].Value.Equals(true))
+                //{
+                //    estaHabilitado = 1;
+                //}
+                //habitacion.SetVista(tieneVista);
+                //habitacion.SetHabilitado(estaHabilitado);
+                //modificarDatosHabitacioncs modifDatos = new modificarDatosHabitacioncs();
+                //modifDatos.RecibirHabitacion(habitacion);
+                //modifDatos.Show();
+            }
+        }
+    }
+}
