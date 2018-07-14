205f52e014ddd9037a75e4cb442cda896949b2c0
jbmild <jbmild@gmail.com>
2018-07-13 23:53:35 -0300

    cambios en hotel

--- FrbaHotel/Hoteles/frmHotelesListado.Designer.cs
+++ FrbaHotel/Hoteles/frmHotelesListado.Designer.cs
@@ -34,153 +34,71 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBoxHotelNombre = new System.Windows.Forms.TextBox();
-            this.comboBoxEstrellas = new System.Windows.Forms.ComboBox();
-            this.textBoxCiudad = new System.Windows.Forms.TextBox();
-            this.textBoxPais = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.dgwHoteles = new System.Windows.Forms.DataGridView();
             this.Modificar = new System.Windows.Forms.DataGridViewButtonColumn();
             this.Eliminar = new System.Windows.Forms.DataGridViewButtonColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cmbEstrellas = new System.Windows.Forms.ComboBox();
+            this.txtCiudad = new System.Windows.Forms.TextBox();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.gpbFiltros = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwHoteles)).BeginInit();
+            this.gpbFiltros.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 72);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.Size = new System.Drawing.Size(69, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre Hotel:";
+            this.label1.Text = "Nombre:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 98);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(12, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.Size = new System.Drawing.Size(74, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Estrellas:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 131);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 112);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(63, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Ciudad:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 158);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(12, 149);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.Size = new System.Drawing.Size(43, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "País:";
             // 
-            // dataGridView1
+            // dgwHoteles
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgwHoteles.AllowUserToAddRows = false;
+            this.dgwHoteles.AllowUserToDeleteRows = false;
+            this.dgwHoteles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwHoteles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Modificar,
             this.Eliminar});
-            this.dataGridView1.Location = new System.Drawing.Point(37, 191);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1082, 122);
-            this.dataGridView1.TabIndex = 8;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(482, 74);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 38);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Buscar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(638, 74);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 38);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Limpiar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBoxHotelNombre
-            // 
-            this.textBoxHotelNombre.Location = new System.Drawing.Point(113, 69);
-            this.textBoxHotelNombre.Margin = new System.Windows.Forms.Padding(2);
-            this.textBoxHotelNombre.Name = "textBoxHotelNombre";
-            this.textBoxHotelNombre.Size = new System.Drawing.Size(205, 20);
-            this.textBoxHotelNombre.TabIndex = 11;
-            // 
-            // comboBoxEstrellas
-            // 
-            this.comboBoxEstrellas.FormattingEnabled = true;
-            this.comboBoxEstrellas.Location = new System.Drawing.Point(113, 96);
-            this.comboBoxEstrellas.Margin = new System.Windows.Forms.Padding(2);
-            this.comboBoxEstrellas.Name = "comboBoxEstrellas";
-            this.comboBoxEstrellas.Size = new System.Drawing.Size(92, 21);
-            this.comboBoxEstrellas.TabIndex = 12;
-            // 
-            // textBoxCiudad
-            // 
-            this.textBoxCiudad.Location = new System.Drawing.Point(113, 127);
-            this.textBoxCiudad.Margin = new System.Windows.Forms.Padding(2);
-            this.textBoxCiudad.Name = "textBoxCiudad";
-            this.textBoxCiudad.Size = new System.Drawing.Size(205, 20);
-            this.textBoxCiudad.TabIndex = 13;
-            // 
-            // textBoxPais
-            // 
-            this.textBoxPais.Location = new System.Drawing.Point(113, 158);
-            this.textBoxPais.Margin = new System.Windows.Forms.Padding(2);
-            this.textBoxPais.Name = "textBoxPais";
-            this.textBoxPais.Size = new System.Drawing.Size(205, 20);
-            this.textBoxPais.TabIndex = 14;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(178, 21);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(731, 24);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "Para comenzar, seleccione un hotel. Puede utilizar los filtros para agilizar su b" +
-    "úsqueda";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(482, 131);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(120, 38);
-            this.button3.TabIndex = 16;
-            this.button3.Text = "Dar de alta hotel";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.dgwHoteles.Location = new System.Drawing.Point(12, 257);
+            this.dgwHoteles.Name = "dgwHoteles";
+            this.dgwHoteles.RowTemplate.Height = 24;
+            this.dgwHoteles.Size = new System.Drawing.Size(973, 363);
+            this.dgwHoteles.TabIndex = 8;
+            this.dgwHoteles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Modificar
             // 
@@ -203,32 +121,100 @@
             this.Eliminar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             this.Eliminar.Text = "Baja";
             // 
-            // modificarHotel
+            // btnBuscar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btnBuscar.Location = new System.Drawing.Point(873, 210);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(112, 41);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(12, 210);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(112, 41);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(100, 33);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(306, 26);
+            this.txtNombre.TabIndex = 11;
+            // 
+            // cmbEstrellas
+            // 
+            this.cmbEstrellas.FormattingEnabled = true;
+            this.cmbEstrellas.Location = new System.Drawing.Point(100, 70);
+            this.cmbEstrellas.Name = "cmbEstrellas";
+            this.cmbEstrellas.Size = new System.Drawing.Size(306, 28);
+            this.cmbEstrellas.TabIndex = 12;
+            // 
+            // txtCiudad
+            // 
+            this.txtCiudad.Location = new System.Drawing.Point(100, 109);
+            this.txtCiudad.Name = "txtCiudad";
+            this.txtCiudad.Size = new System.Drawing.Size(306, 26);
+            this.txtCiudad.TabIndex = 13;
+            // 
+            // txtPais
+            // 
+            this.txtPais.Location = new System.Drawing.Point(100, 146);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(306, 26);
+            this.txtPais.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(873, 626);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 41);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Alta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // gpbFiltros
+            // 
+            this.gpbFiltros.Controls.Add(this.label1);
+            this.gpbFiltros.Controls.Add(this.label2);
+            this.gpbFiltros.Controls.Add(this.txtPais);
+            this.gpbFiltros.Controls.Add(this.label3);
+            this.gpbFiltros.Controls.Add(this.txtCiudad);
+            this.gpbFiltros.Controls.Add(this.label4);
+            this.gpbFiltros.Controls.Add(this.cmbEstrellas);
+            this.gpbFiltros.Controls.Add(this.txtNombre);
+            this.gpbFiltros.Location = new System.Drawing.Point(12, 12);
+            this.gpbFiltros.Name = "gpbFiltros";
+            this.gpbFiltros.Size = new System.Drawing.Size(973, 192);
+            this.gpbFiltros.TabIndex = 17;
+            this.gpbFiltros.TabStop = false;
+            this.gpbFiltros.Text = "Filtros";
+            // 
+            // frmHotelesListado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1028, 329);
+            this.ClientSize = new System.Drawing.Size(1000, 683);
+            this.Controls.Add(this.gpbFiltros);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBoxPais);
-            this.Controls.Add(this.textBoxCiudad);
-            this.Controls.Add(this.comboBoxEstrellas);
-            this.Controls.Add(this.textBoxHotelNombre);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "modificarHotel";
-            this.Text = "Modificar hotel";
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgwHoteles);
+            this.Name = "frmHotelesListado";
+            this.Text = "Administracion de hoteles";
             this.Load += new System.EventHandler(this.frmHotelesListado_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwHoteles)).EndInit();
+            this.gpbFiltros.ResumeLayout(false);
+            this.gpbFiltros.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -238,16 +224,16 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBoxHotelNombre;
-        private System.Windows.Forms.ComboBox comboBoxEstrellas;
-        private System.Windows.Forms.TextBox textBoxCiudad;
-        private System.Windows.Forms.TextBox textBoxPais;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgwHoteles;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cmbEstrellas;
+        private System.Windows.Forms.TextBox txtCiudad;
+        private System.Windows.Forms.TextBox txtPais;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridViewButtonColumn Modificar;
         private System.Windows.Forms.DataGridViewButtonColumn Eliminar;
+        private System.Windows.Forms.GroupBox gpbFiltros;
     }
 }
--- FrbaHotel/Hoteles/frmHotelesListado.cs
+++ FrbaHotel/Hoteles/frmHotelesListado.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FrbaHotel.Hoteles.Modelo;
+using FrbaHotel.Tools;
 
 
 namespace FrbaHotel.Hoteles
@@ -25,75 +27,39 @@ namespace FrbaHotel.Hoteles
 
         private void frmHotelesListado_Load(object sender, EventArgs e)
         {
-            ConexionSQL c = new ConexionSQL();
-            DataTable estrellas = c.cargarTablaSQL("select distinct estrellas_cant from WHERE_EN_EL_DELETE_FROM.hoteles");
-            estrellas.Rows.InsertAt(estrellas.NewRow(),0);
-            comboBoxEstrellas.DataSource=estrellas;    
-            comboBoxEstrellas.SelectedIndex=0;
-            comboBoxEstrellas.DisplayMember="estrellas_cant";
-            comboBoxEstrellas.ValueMember="estrellas_cant";
+            List<int> estrellas = Estrellas.obtener();
+
+            cmbEstrellas.Items.Clear();
+            foreach (int estrella in estrellas)
+            {
+                cmbEstrellas.Items.Add(new Option(estrella.ToString(), estrella.ToString()));
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnBuscar_Click(object sender, EventArgs e)
         {
-            int nombreNoVacio=0;
-            int estrellasNoVacio=0;
-            int ciudadNoVacio=0;
-            
-            ConexionSQL c = new ConexionSQL();
-            string consulta = "select hotel_id, isNull(nombre, 'hotel ' + direccion) as 'nombre', mail, telefono, direccion, ciudad, pais, estrellas_cant, estrellas_recargo, convert(varchar(10), fecha_creacion, 110) As [Fecha Creacion] from WHERE_EN_EL_DELETE_FROM.hoteles";
-            if(textBoxHotelNombre.Text.Equals("")){}else{nombreNoVacio=1; consulta+=" where nombre like '%" + textBoxHotelNombre.Text + "%'";}
-            if(comboBoxEstrellas.Text.Equals("")){}else
+            int estrellas = 0;
+            if (cmbEstrellas.SelectedIndex != -1)
             {
-                estrellasNoVacio=1;
-                if(nombreNoVacio.Equals(1))
-                {
-                    consulta+=" and estrellas_cant=" + comboBoxEstrellas.SelectedValue;
-                }else
-                {
-                    consulta+=" where estrellas_cant=" + comboBoxEstrellas.SelectedValue;
-                }
+                Option opcion = (Option)cmbEstrellas.Items[cmbEstrellas.SelectedIndex];
+                estrellas = Convert.ToInt32(opcion.Value);
             }
-            if(textBoxCiudad.Text.Equals("")){}else
-            {
-                ciudadNoVacio=1;
-                if (estrellasNoVacio.Equals(1) || nombreNoVacio.Equals(1))
-                {
-                    consulta+=" and ciudad like '%" + textBoxCiudad.Text + "%'";
-                }
-                else
-                {
-                    consulta+=" where ciudad like '%" + textBoxCiudad.Text + "%'";
-                }
-            }
-            if (textBoxPais.Text.Equals("")){}else
-            {
-                if (estrellasNoVacio.Equals(1) || nombreNoVacio.Equals(1) || ciudadNoVacio.Equals(1))
-                {
-                    consulta+=" and pais like '%" + textBoxPais.Text + "%'";
-                }
-                else
-                {
-                    consulta+= " where pais like '%" + textBoxPais.Text + "%'";
-                }
-            }
-            DataTable resultados = c.cargarTablaSQL(consulta);
-            dataGridView1.DataSource = resultados;
+
+            this.CargarGrilla(Hoteles.Modelo.Hoteles.obtener(txtNombre.Text, txtCiudad.Text, txtPais.Text, estrellas));
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnLimpiar_Click(object sender, EventArgs e)
         {
-            textBoxPais.Text = "";
-            textBoxHotelNombre.Text = "";
-            textBoxCiudad.Text = "";
-            comboBoxEstrellas.SelectedIndex = 0;
+            txtPais.Text = "";
+            txtNombre.Text = "";
+            txtCiudad.Text = "";
+            cmbEstrellas.SelectedIndex = -1;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            int row = e.RowIndex;
-            DataGridViewRow selectedRow = dataGridView1.Rows[row];
+            DataGridViewRow selectedRow = dgwHoteles.Rows[e.RowIndex];
 
             if (e.ColumnIndex.Equals(0))
             {
@@ -116,17 +82,7 @@ namespace FrbaHotel.Hoteles
                 datosHotel.RecibirHotel(hotel);
                 datosHotel.Show();
                 this.Hide();
-
-                //hotel.SetNombre(selectedRow.Cells[3].Value.ToString());
-                //habitacion.SetTipo(selectedRow.Cells[4].Value.ToString());
-                //habitacion.SetDireccion(selectedRow.Cells[7].Value.ToString());
-                //habitacion.SetHabiID(Int32.Parse(selectedRow.Cells[8].Value.ToString()));
-                //habitacion.SetHotelID(this.ObtenerHotelID(habitacion));
-
-
-              
-            }
-            if (e.ColumnIndex.Equals(1)) {
+            }else if (e.ColumnIndex.Equals(1)) {
 
                 bajaHotel frmBaja = new bajaHotel(Int32.Parse(selectedRow.Cells[2].Value.ToString()));
                 frmBaja.Show();
@@ -136,15 +92,13 @@ namespace FrbaHotel.Hoteles
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (abm.GetVentanaAbierta().Equals(1)) 
-            {
-                MessageBox.Show("No se puede dar de alta un hotel mientras la ventana de edición está abierta");
-            }
-            else
-            {
-                altaHotel alta = new altaHotel();
-                alta.Show();
-            }
+            altaHotel alta = new altaHotel();
+            alta.ShowDialog();
+        }
+
+        private void CargarGrilla(DataTable data)
+        {
+            dgwHoteles.DataSource = data;
         }
 
        
